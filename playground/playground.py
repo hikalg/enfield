a2d100cadf4a1fed07bc3a24754ea2852769f689
Everest Luong <101264127+hikalg@users.noreply.github.com>
2025-10-09 21:38:43 +1100

    settings

---
+++ playground/playground.py
@@ -0,0 +1,8 @@
+from enfield import settings as s
+
+team_settings = s.TeamSettings()
+match_settings = s.MatchSettings()
+
+print(team_settings.maximum_team_size)
+
+print(match_settings.maximum_team_size)
