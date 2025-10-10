b1790d7cb86e87ac6f7b36bbba351aed6991b19f
Everest Luong <101264127+hikalg@users.noreply.github.com>
2025-10-10 20:03:45 +1100

    added settings initialisation

--- enfield/settings/__init__.py
+++ enfield/settings/__init__.py
@@ -1,1 +1,7 @@
-from .settings import BasicSettings, TeamSettings, RatingSettings, MatchSettings
+from .meta_settings import MetaSettings
+from .player_settings import PlayerSettings
+from .rating_settings import RatingSettings
+
+# the big meeg
+class Settings(MetaSettings, PlayerSettings, RatingSettings):
+    pass
