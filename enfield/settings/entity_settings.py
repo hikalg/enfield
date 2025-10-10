5c36dcc52eb95d29fad44d96b3f1b10b4924d76c
Everest Luong <101264127+hikalg@users.noreply.github.com>
2025-10-10 21:13:43 +1100

    reformatting

--- enfield/settings/entity_settings.py
+++ enfield/settings/entity_settings.py
@@ -1,18 +1,10 @@
 from typing import Annotated
 from pydantic import Field, Strict
-from pydantic.types import (
-    StringConstraints,
-)
-
-from pydantic_extra_types.country import (
-    CountryAlpha2
-)
-
-from pydantic_extra_types.timezone_name import (
-    TimeZoneName
-)
-
+from pydantic.types import StringConstraints
+from pydantic_extra_types.country import CountryAlpha2
+from pydantic_extra_types.timezone_name import TimeZoneName
 from pydantic_settings import BaseSettings
+
 
 class EntitySettings(BaseSettings):
     entity_default_name: Annotated[
@@ -23,25 +15,20 @@ class EntitySettings(BaseSettings):
             min_length=2, max_length=16, strip_whitespace=True, to_lower=True
         ),
     ] = "player"
-    
+
     entity_default_description: Annotated[
         str,
         Strict(),
         Field(alias="e_desc_def", default=""),
-        StringConstraints(
-            max_length=2048
-        )
-    ] = ""
-    
+        StringConstraints(max_length=2048),
+    ] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at semper arcu. Phasellus a laoreet turpis."
+
     entity_default_country: Annotated[
-        CountryAlpha2,
-        Field(alias="e_country_def", default='AU')
-    ] = 'AU'
-    
-    entity_default_timezone : Annotated[
-        TimeZoneName,
-        Field(alias="e_timezone_default", default='Australia/Sydney')
-    ] = 'Australia/Sydney'
-    
-print(EntitySettings().model_dump())
-    
+        CountryAlpha2, Field(alias="e_country_def", default="AU")
+    ] = "AU"
+
+    entity_default_timezone: Annotated[
+        TimeZoneName, Field(alias="e_timezone_default", default="Australia/Sydney")
+    ] = "Australia/Sydney"
+
+
